0aa1cd720569f3bd46559274793ff8018a64580e
E <logsdonevan1@gmail.com>
2025-05-12 22:03:06 -0700

    zip donwload/upload working

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,7 +2,7 @@ use std::time::Instant;
 
 use axum::{
     body::Bytes,
-    extract::{Path, Request, State},
+    extract::{Path, State},
     http::{header::CONTENT_TYPE, HeaderMap, HeaderValue, StatusCode},
     response::IntoResponse,
     Json,
@@ -26,8 +26,24 @@ pub async fn get_file<R: Repository>(
     State(state): State<AppState<R>>,
     Path(id): Path<String>,
 ) -> impl IntoResponse {
+    let start: Instant = Instant::now();
+
     let span: Span = span!(Level::INFO, "get_file", context = "get_file");
     let _e = span.enter();
+
+    let file: Vec<u8> = match state.repo.get(&id).await {
+        Err(err) => {
+            error!(error.kind="get", error.message = %err, %id, "failed");
+            return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+        }
+        Ok(f) => f,
+    };
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static(ZIP_TYPE));
+
+    info!(info.kind="get", elapsed = start.elapsed().as_millis(), unit = "ms", %id, "success");
+    (StatusCode::OK, headers, Bytes::from(file)).into_response()
 }
 
 pub async fn put_file<R: Repository>(
@@ -35,13 +51,12 @@ pub async fn put_file<R: Repository>(
     headers: HeaderMap,
     body: Bytes,
 ) -> impl IntoResponse {
-    let start = Instant::now();
+    let start: Instant = Instant::now();
 
     let span: Span = span!(Level::INFO, "put_file", context = "put_file");
     let _e = span.enter();
 
     let content_type: Option<&HeaderValue> = headers.get(CONTENT_TYPE);
-    // TODO unwrap()
     if content_type != Some(&ZIP_TYPE.parse().unwrap()) {
         warn!(
             warn.kind = "put",
@@ -52,15 +67,15 @@ pub async fn put_file<R: Repository>(
         return (StatusCode::UNSUPPORTED_MEDIA_TYPE).into_response();
     }
 
-    let uuid = Uuid::new_v4().to_string();
-    match state.repo.put(&uuid, body.as_ref()).await {
+    let id = Uuid::new_v4().to_string();
+    match state.repo.put(&id, body.as_ref()).await {
         Err(err) => {
-            error!(error.kind="put", error.message = %err, %uuid, "failed");
+            error!(error.kind="put", error.message = %err, %id, "failed");
             return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
         }
         Ok(()) => {}
     }
 
-    info!(info.kind="put", elapsed = start.elapsed().as_millis(), %uuid, "success");
+    info!(info.kind="put", elapsed = start.elapsed().as_millis(), unit = "ms", %id, "success");
     (StatusCode::CREATED).into_response()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::sync::Arc;
 
 use axum::{
-    extract::DefaultBodyLimit, routing::{get, put}, Router
+    extract::DefaultBodyLimit,
+    routing::{get, put},
+    Router,
 };
 use model::{AppState, S3Repo};
 use tokio::net::TcpListener;
-use tower_http::{limit::RequestBodyLimitLayer, trace::TraceLayer};
+use tower_http::trace::TraceLayer;
 use tracing::{info, span, Level, Span};
 
 mod handlers;
@@ -19,7 +21,10 @@ async fn main() {
 
     let port: String = std::env::var("PORT").unwrap_or("3000".to_string());
     let bucket: String = std::env::var("BUCKET").unwrap();
-    let body_limit: usize = std::env::var("BODY_LIMIT_MB").unwrap_or("20".to_string()).parse().unwrap();
+    let body_limit: usize = std::env::var("BODY_LIMIT_MB")
+        .unwrap_or("20".to_string())
+        .parse()
+        .unwrap();
 
     let state: AppState<S3Repo> = AppState {
         repo: Arc::new(S3Repo::new(bucket).await),
@@ -28,7 +33,10 @@ async fn main() {
         .route("/healthz", get(handlers::healthz))
         .route("/readyz", get(handlers::readyz))
         .route("/api/file/{id}", get(handlers::get_file))
-        .route("/api/file", put(handlers::put_file).layer(DefaultBodyLimit::max(body_limit * 1024 * 1024)))
+        .route(
+            "/api/file",
+            put(handlers::put_file).layer(DefaultBodyLimit::max(body_limit * 1024 * 1024)),
+        )
         .layer(TraceLayer::new_for_http())
         .with_state(state);
 
